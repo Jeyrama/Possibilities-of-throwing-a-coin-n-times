c788c9773b43b7b292442d0b4b3ef6ce3c2f3ea0
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-05-05 22:30:19 -0700

    Create Possibilities of throwing a coin n times.js

---
+++ Possibilities of throwing a coin n times.js
@@ -0,0 +1,19 @@
+/*
+In this challenge you will be given an integer n, 
+which is the number of times that is thown a coin. 
+You will have to return an array of string for 
+all the possibilities (heads[H] and tails[T]). 
+
+Examples:
+  coin(1) should return {"H", "T"}
+  coin(2) should return {"HH", "HT", "TH", "TT"}
+  coin(3) should return {"HHH", "HHT", "HTH", "HTT", "THH", "THT", "TTH", "TTT"}
+
+When finished sort them alphabetically.
+
+In C and C++ just return a char* with all elements separated by, (without space):
+coin(2) should return "HH,HT,TH,TT"
+
+INPUT:
+  0 < n < 18
+*/
